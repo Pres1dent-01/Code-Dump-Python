bb919854475438bd1350d9f3b6475d776fe6beb0
Vale <51172366+Pres1dent-01@users.noreply.github.com>
2026-08-30 19:49:42 +0100

    Add files via upload

---
+++ Striver-Pattern-6.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+using namespace std;
+
+int main(){
+    int n=5;
+    
+    for(int i=1; i<=n; i++){
+        int k=1;
+        for(int j=n; j>=i; j--, k++){
+            cout << k;
+        }
+        cout << endl;
+    }
+}
